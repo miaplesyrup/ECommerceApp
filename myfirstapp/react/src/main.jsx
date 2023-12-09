5faeb4e611e48a29247bdefc9ebfebe8be350c7d
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-12-09 21:05:34 +0800

    Signup and Login

--- myfirstapp/react/src/main.jsx
+++ myfirstapp/react/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { ContextProvider } from './assets/components/contexts/ContextProvider.jsx'
 import './index.css'
 import router from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </React.StrictMode>,
 )
--- myfirstapp/react/src/views/Signup.jsx
+++ myfirstapp/react/src/views/Signup.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom"
+
 export default function Signup() {
+
+  const onSubmit = (ev) => {
+    ev.preventDefault()
+    // alert('Signup Successful!')
+  }
+
+
   return (
-    <div>Signup</div>
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">
+            Signup for free
+          </h1>
+          <input type="email" placeholder="Full Name"/>
+          <input type="email" placeholder="Email Address"/>
+          <input type="password" placeholder="Password"/>
+          <input type="password" placeholder="Password Confirmation"/>
+          <button className="btn btn-block">Signup</button>
+          <p className="message">
+            Already registered? <Link to="/login">Sign in</Link>
+          </p>
+        </form>
+      </div>
+      Login
+    </div>
   )
 }
